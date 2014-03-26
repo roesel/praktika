914bb82c5b75d082c4ff2087406e4c059f6b838a
David Roesel <roesel@gmail.com>
2014-03-26 20:14:11 +0100

    GolemBot updated :)

--- ZS_2014/13b_toka/python/golbot.py
+++ ZS_2014/13b_toka/python/golbot.py
@@ -3,10 +3,26 @@
 
 from __future__ import division
 from itertools import izip
+import matplotlib.pyplot as plt
+from scipy import *
+from scipy import optimize
 import math
-import sys
 import urllib2
 import os
+
+# ---- TADY SE UPRAVUJE ----
+
+# 1. jaké shoty chcete zpracovávat?
+shot_numbers = "14681 14683 14685 14709 14710 14711 14712"
+
+# 2. jaká jsou minima a maxima intervalu casu, ve kterych chcete pocitat (v poradi shotu)?
+mins          = "0.0225 0.02475 0.02445 0.0225 0.0225 0.0225 0.0225" 
+maxs          = "0.0230 0.02525 0.02495 0.0230 0.0230 0.0230 0.0230"
+
+# 3. jaké je maximum napětí, na kterém se fitují V-A charakteristiky?
+maxU          = -30
+
+# ---- KONEC UPRAVOVANI ----
 
 flag_redownload = False
 
@@ -57,10 +73,6 @@ data_names = ['loop_voltage',
               'rake_probe_2', 
               'rake_probe_3']
 
-# seznam cisel shotu, ktere me zajimaji   
-shot_numbers = "14681 14683 14685 14709 14710 14711 14712"
-mins          = "0.0225 0.02475 0.02445 0.0225 0.0225 0.0225 0.0225"
-maxs          = "0.0230 0.02525 0.02495 0.0230 0.0230 0.0230 0.0230"
 
 shot_numbers = shot_numbers.split(" ")
 mins = mins.split(" ")
@@ -112,6 +124,8 @@ for i in range(0,len(shot_numbers)):
     fo = open(output_name, "wb")
     fo.write(output);
     fo.close()
+    
+    # dost mozna vubec nepotrbuju output    
 
 fo = open('output/T_merge__output.txt', "wb")
 fo.write(output2);
@@ -121,7 +135,9 @@ print("\n----\nTemperatures calculated and files merged!\n----\n")
         
 # 3. merge VA 
 # ---------------------------
-
+k=0
+if not os.path.exists('graphs'):
+    os.makedirs('graphs')
 for shot_number in shot_numbers:
     folder = 'files'
     file_1 = folder+'/rake_probe_3_'+str(shot_number)+'.txt'
@@ -130,13 +146,61 @@ for shot_number in shot_numbers:
     
     output = '#t[ms]      U[V]      I[A]\n'
     
+    t = []
+    U = []
+    I = []
+    
     for line1, line2 in izip(open(file_1), open(file_2)):
-        output += line1.split()[0] + "	" + line1.split()[1] + "	" + line2.split()[1] + "\n"
+        t_ = line1.split()[0]
+        t.append(t_)
+        u = line1.split()[1]
+        U.append(float(u)*100)
+        i = line2.split()[1]
+        I.append(float(i)/22)
+        output += t_ + "	" + u + "	" + i + "\n"
     
     # Open a file
     fo = open(output_name, "wb")
     fo.write(output)
     fo.close()
-    print("File "+output_name+" sucessfully saved!")
+    #print("File "+output_name+" sucessfully saved!")
+    
+    
+    # Finished outputting file for GNUplot, time to pyplot ourselves
+    max_s = int(float(maxs[k])*10**6)
+    min_s = int(float(mins[k])*10**6)
 
+    # Fit the first set
+    fitfunc = lambda p, x: p[0]*(1-exp((x-p[1])/p[2]))  # Target function
+    errfunc = lambda p, x, y: fitfunc(p, x) - y  # Distance to the target function
+    p0 = [0.011, -53.2, 16] # Initial guess for the parameters
+    
+    subUt = U[min_s:max_s]
+    subU = [subUt[i] for i in range(0, len(subUt)) if subUt[i] <= maxU]
+    subIt = I[min_s:max_s]
+    subI = [subIt[i] for i in range(0, len(subUt)) if subUt[i] <= maxU]
+    
+    p1,cov,infodict,mesg,ier = optimize.leastsq(errfunc, p0[:], args=(subU, subI), full_output=True)
+    
+    #plt.clf()
+    plt.figure(k+1)
+    plt.legend(["V-A char. pro #"+shot_number, "fit   C="+str(p1[2])])
+    print("V-A char. T (fit) for shot #"+shot_number+" is ("+"{:5.2f}".format(p1[2])+" +- ??)")
+    
+    #print p1
+    
+    line1 = plt.plot(subUt, subIt, linestyle='--', linewidth=0.1, antialiased=True)
+    line2 = plt.plot(subUt, fitfunc(p1, subUt), linewidth=0.1, antialiased=True)
+    plt.setp(line1, color='y', linewidth=1.0)
+    plt.setp(line2, color='g', linewidth=1.0)
+    plt.ylabel('I [A]')
+    plt.xlabel('U [V]')
+    plt.savefig('graphs/VA_merge_'+str(shot_number)+'.png')
+    
+    #plt.show()    
+    
+    k+=1
+    
+    #max_s = int(float(maxs[k])*10**6)
+    
 print("\n----\nVA files merged!\n----\n")
